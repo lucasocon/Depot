0bd1d4c63d79f8fa4fd6e1a17b55e5a357a9ddac
Lucas Ocon <lucas_masterclas@hotmail.com>
2014-10-04 18:52:28 -0300

    funciona todo

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Product.create!(title: 'Programming Ruby 1.9 & 2.0',
 	%{<p>
 		Ruby is the fastest growing and most exciting dynamic language
 		out there. If you need to get working programs delivered fastest
-		you shpuld add Ruby to your toolbox.
+		you shpuld add Ruby to your toolboxx.
 		</p>},
 	image_url: 'ruby.jpg',
 	price:49.95)
